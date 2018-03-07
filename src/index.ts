da03d4c8a1401d8c7ae6f1bfe0698817dabb627f
Michael Ardan <michael@Michaels-MacBook-Pro.local>
2018-03-07 23:48:39 +0800

    Created BarChart Service

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,21 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
+import { MfBarChartService } from './mf-bar-chart.service';
 
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
+export * from './mf-bar-chart.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
-  ],
-  exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
-  ]
+  declarations: [],
+  exports: []
 })
 export class SampleModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SampleModule,
-      providers: [SampleService]
+      providers: [MfBarChartService]
     };
   }
 }
